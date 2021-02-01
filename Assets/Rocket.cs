55558a60dddcadff899d784f73216b9f2caba32f
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-02-01 15:46:56 +0530

    added basic thrusting feature to rocket

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
@@ -20,7 +21,7 @@ public class Rocket : MonoBehaviour
     private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space)) {
-            print("Thrusting..");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
         if (Input.GetKey(KeyCode.A))
         {
