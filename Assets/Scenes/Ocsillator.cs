dfc79cdd6798e9d067d06413bd003faf0c00ed5d
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-02-02 22:44:44 +0530

    created oscillator script to move barriers in game..

---
+++ Assets/Scenes/Ocsillator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Ocsillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    //TODO remove from inspector
+    Vector3 startingPosition;
+    [Range(0,1)]
+    [SerializeField] float movementFactor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = startingPosition + movementVector * movementFactor;
+        transform.position = newPosition;
+    }
+}
