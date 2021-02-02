a90741b16d840a26147a83a0d6b6f7e47528e359
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-02-02 23:38:28 +0530

    modifided Ocsillator script for automate simple harmonic motion for obstacles

--- Assets/Scenes/Ocsillator.cs
+++ Assets/Scenes/Ocsillator.cs
@@ -3,11 +3,12 @@
 [DisallowMultipleComponent]
 public class Ocsillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector=new Vector3(10f,10f,10f);
     //TODO remove from inspector
     Vector3 startingPosition;
-    [Range(0,1)]
-    [SerializeField] float movementFactor;
+    [Range(0,1)][SerializeField] float movementFactor;
+    [SerializeField]private float period=2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,11 @@ public class Ocsillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period<=Mathf.Epsilon) { throw new System.DivideByZeroException();}
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = (rawSinWave / 2f) + 0.5f;
         Vector3 newPosition = startingPosition + movementVector * movementFactor;
         transform.position = newPosition;
     }
