a13540e8bbba64b256f91089b26d2a3a1b84ab83
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-02-02 08:57:41 +0530

    added thrustin sound to rocketship

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>(); 
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,9 +22,13 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space)) {
+        if (Input.GetKey(KeyCode.Space))
+        {
             rigidBody.AddRelativeForce(Vector3.up);
-            print("Thrusting");
+            if (!audioSource.isPlaying) { audioSource.Play(); }
+        }
+        else {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
